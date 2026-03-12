8263ae346da260f3b8397d0ad9ae592c00f9f41b
Ruben Valencia Arancibia <rvalencia@inacap.cl>
2026-03-12 16:12:29 -0300

    feat: add deply and minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: 'https://rueeen.github.io',
+  base: '/astro-test'
 });
+
